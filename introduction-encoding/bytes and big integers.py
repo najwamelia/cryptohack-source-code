35b3a148e454cc27229846b9ca646f0223de9b8e
najwamelia <99813716+najwamelia@users.noreply.github.com>
2022-03-18 09:56:13 +0700

    task1

---
+++ introduction-encoding/bytes and big integers.py
@@ -0,0 +1,7 @@
+# Najwa Amelia Qorry 'Aina - 5027201001
+#!/usr/bin/env python3
+
+from Crypto.Util.number import bytes_to_long
+from Crypto.Util.number import long_to_bytes
+
+print(long_to_bytes(11515195063862318899931685488813747395775516287289682636499965282714637259206269))
