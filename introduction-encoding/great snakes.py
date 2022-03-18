7ebc27f7d0efa4d4c83999d918733cd0f85bb80a
najwamelia <99813716+najwamelia@users.noreply.github.com>
2022-03-18 09:48:50 +0700

    task1

---
+++ introduction-encoding/great snakes.py
@@ -0,0 +1,13 @@
+# Najwa Amelia Qorry 'Aina - 5027201001
+#!/usr/bin/env python3
+
+import sys
+# import this
+
+if sys.version_info.major == 2:
+    print("You are running Python 2, which is no longer supported. Please update to Python 3.")
+
+ords = [81, 64, 75, 66, 70, 93, 73, 72, 1, 92, 109, 2, 84, 109, 66, 75, 70, 90, 2, 92, 79]
+
+print("Flag:")
+print("".join(chr(o ^ 0x32) for o in ords))
