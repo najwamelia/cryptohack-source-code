33cfef61e071356b791a4a06f6c5ec6a3cff9096
najwamelia <99813716+najwamelia@users.noreply.github.com>
2022-03-18 09:54:38 +0700

    task1

---
+++ introduction-encoding/ASCII.py
@@ -0,0 +1,11 @@
+# Najwa Amelia Qorry 'Aina - 5027201001
+#!/usr/bin/env python3
+
+import sys
+
+ords = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+
+conv=''
+for i in ords: conv += chr(i)
+
+print(conv)
