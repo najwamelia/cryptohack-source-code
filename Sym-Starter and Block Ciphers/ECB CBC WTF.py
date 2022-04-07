422cf3629bc618e0b429f2646f0707e0cadb35f8
najwamelia <99813716+najwamelia@users.noreply.github.com>
2022-04-08 06:59:14 +0700

    task 5

--- Sym-Starter and Block Ciphers/ECB CBC WTF.py
+++ Sym-Starter and Block Ciphers/ECB CBC WTF.py
@@ -5,10 +5,10 @@ from tkinter import N
 from Crypto.Cipher import AES
 import requests
 
-def xor(a, b):
-	a = bytes.fromhex(a)
-	b = bytes.fromhex(b)
-	return bytes(i ^ j for i, j in zip(a, b))
+def xor(m, n):
+	m = bytes.fromhex(m)
+	n = bytes.fromhex(n)
+	return bytes(i ^ j for i, j in zip(m, n))
 
 def decrypt(ciphertext):
     ciphertext = bytes.fromhex(ciphertext)
@@ -25,18 +25,16 @@ def decrypt(ciphertext):
 cipher = requests.get('http://aes.cryptohack.org/ecbcbcwtf/encrypt_flag/')
 cipher = cipher.json()['ciphertext']
 
-iv = cipher[:32]
+a = cipher[:32]
 cipher1 = cipher[32:64]
 cipher2 = cipher[64:96]
-
 decrypt2 = requests.get('http://aes.cryptohack.org/ecbcbcwtf/decrypt/' + cipher2 + '/')
 decrypt2 = decrypt2.json()['plaintext']
 decrypt2 = xor(decrypt2,cipher1)
-
 decrypt1 = requests.get('http://aes.cryptohack.org/ecbcbcwtf/decrypt/' + cipher1 + '/')
 decrypt1 = decrypt1.json()['plaintext']
-decrypt1 = xor(decrypt1,iv)
+decrypt1 = xor(decrypt1, a)
 
-print(decrypt1+decrypt2)
+print(decrypt1 + decrypt2)
 
 # flag  crypto{3cb_5uck5_4v01d_17_!!!!!}
