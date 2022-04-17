b3954d558de5f3cd1cc9cc66cd010900cc3d6aa4
najwamelia <99813716+najwamelia@users.noreply.github.com>
2022-04-17 09:56:17 +0700

    task 6

--- Block Ciphers and Stream Ciphers/symmetry.py
+++ Block Ciphers and Stream Ciphers/symmetry.py
@@ -1,1 +1,24 @@
+# Najwa Amelia Qorry 'Aina - 5027201001
+#!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
+import requests
+
+ct_hex = requests.get('http://aes.cryptohack.org/symmetry/encrypt_flag/').json()
+ct = ct_hex['ct']
+print(ct)
+
+n = ct[:16*2]
+print(n)
+ct = ct[32:]
+print(ct)
+
+pt_hex = requests.get('http://aes.cryptohack.org/symmetry/encrypt/'+ct+'/'+n+'/').json()
+pt = pt_hex['ct']
+print(pt)
+
+flag = bytes.fromhex(pt)
+print(flag)
+
+# flag = crypto{0fb_15_5ymm37r1c4l_!!!11!}
